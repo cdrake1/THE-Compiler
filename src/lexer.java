0b4e2bd2f607b5a116d7fca5552c86c2202226ee
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-01-25 08:45:26 -0500

    reading the text file in worked

--- src/lexer.java
+++ src/lexer.java
@@ -1,11 +1,38 @@
 /*
     Lexical Analysis
 */
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Lexer {
+public class Lexer 
+{
     public Lexer(){}
 
-    public ArrayList readFile(String inputFile){}
-    
+    public void readFile(String inputFile)
+    {
+
+        ArrayList<String> textFile = new ArrayList<>();
+
+        try
+        {
+            Scanner scanner = new Scanner(new File(inputFile));
+            while(scanner.hasNextLine()){
+                textFile.add(scanner.nextLine());
+            }
+            scanner.close();
+        }
+        catch(IOException exception)
+        {
+            System.out.println("Something went wrong when trying to read the file");
+        }
+
+        for(int i = 0; i < textFile.size(); i++){
+            System.out.println(textFile.get(i));
+        }
+    }
 }
