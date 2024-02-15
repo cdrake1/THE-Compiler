45643deae71a0503ed136dd1540abed3424d9fab
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-02-15 09:21:03 -0500

    setting up the layout for the parser. Added functions, but havent built them out yet.

--- src/Parser.java
+++ src/Parser.java
@@ -19,9 +19,106 @@ public class Parser{
         System.out.println("PARSER - " + output);
     }
 
-    public void parseProgram(ArrayList<Token> programTokenStream){
-        for(int i = 0; i < programTokenStream.size(); i++){
-            parserLog(programTokenStream.get(i).tokenType);
-        }
+    //calls the other functions to parse the tokenstream and build out the CST
+    public void parseTokenStream(ArrayList<Token> programTokenStream){}
+
+    private void parseProgram(){
+        match();
     }
+
+    private void parseBlock(){
+        match();
+    }
+
+    private void parseStatementList(){
+        match();
+    }
+
+    private void parseStatement(){
+        match();
+    }
+
+    private void parsePrintStatement(){
+        match();
+    }
+
+    private void parseAssignmentStatement(){
+        match();
+    }
+
+    private void parseVarDecl(){
+        match();
+    }
+
+    private void parseWhileStatement(){
+        match();
+    }
+
+    private void parseIfStatement(){
+        match();
+    }
+
+    private void parseExpr(){
+        match();
+    }
+
+    private void parseIntExpr(){
+        match();
+    }
+
+    private void parseStringExpr(){
+        match();
+    }
+
+    private void parseBooleanExpr(){
+        match();
+    }
+
+    //parse ID which is a char
+    private void parseID(){
+        match();
+    }
+
+    //parse charlist: char, space, Empty statement
+    private void parseCharList(){
+        //switch or if to check if char space or empty
+        match();
+    }
+
+    //parse types: int, string, bool
+    private void parseType(){
+        match();
+    }
+
+    //parse chars a-z
+    private void parseChar(){
+        match();
+    }
+
+    //parse whitespace
+    private void parseSpace(){
+        match();
+    }
+
+    //parse digits 0-9
+    private void parseDigit(){
+        match();
+    }
+
+    //parse bool operators
+    private void parseBoolOp(){
+        match();
+    }
+
+    //parse bool values
+    private void parseBoolVal(){
+        match();
+    }
+
+    //parse add op (+)
+    private void parseIntOp(){
+        match();
+    }
+
+    private void match(){}
 }
