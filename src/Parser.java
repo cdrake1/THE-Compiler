609d6a7765903283d2ebc31a0012e7782bc33632
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-02-15 20:01:27 -0500

    moved and organized files within repo. Added comments to parser.

--- src/Parser.java
+++ src/Parser.java
@@ -10,9 +10,14 @@ import java.util.ArrayList;
 
 //Collin Drakes Parser
 public class Parser{
+    int parseError; //counts the number of parse errors
+    int parseWarnings;   //counts the number of parse warnings
 
     //creates a parser and initializes all variables. We are prepared to start parsing!
-    public Parser(){}
+    public Parser(){
+        this.parseError = 0;
+        this.parseWarnings = 0;
+    }
 
     //outputs the results of the parser. Also outputs warnings and errors...
     private void parserLog(String output){
@@ -22,59 +27,74 @@ public class Parser{
     //calls the other functions to parse the tokenstream and build out the CST
     public void parseTokenStream(ArrayList<Token> programTokenStream){}
 
+    //parse program
     private void parseProgram(){
+        parserLog("Parsing program");
+        parseBlock();
         match();
     }
 
+    //parse block
     private void parseBlock(){
         match();
     }
 
+    //parse statement list
     private void parseStatementList(){
         match();
     }
 
+    //parse statement
     private void parseStatement(){
         match();
     }
 
+    //parse print statement
     private void parsePrintStatement(){
         match();
     }
 
+    //parse assignment statement
     private void parseAssignmentStatement(){
         match();
     }
 
+    //parse variable declaration
     private void parseVarDecl(){
         match();
     }
 
+    //parse while statement
     private void parseWhileStatement(){
         match();
     }
 
+    //parse if statement
     private void parseIfStatement(){
         match();
     }
 
+    //parse expression
     private void parseExpr(){
         match();
     }
 
+    //parse int expression
     private void parseIntExpr(){
         match();
     }
 
+    //parse string expression
     private void parseStringExpr(){
         match();
     }
 
+    //parse boolean expression
     private void parseBooleanExpr(){
         match();
     }
 
-    //parse ID which is a char
+    //parse ID
     private void parseID(){
         match();
     }
@@ -120,5 +140,6 @@ public class Parser{
         match();
     }
 
+    //matches and consumes tokens
     private void match(){}
 }
