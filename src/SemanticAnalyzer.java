99c834672b6715290eeb9922e4278c22c36d4aff
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-03-22 19:55:20 -0400

    order needs to be fixed still. Only for  ops though

--- src/SemanticAnalyzer.java
+++ src/SemanticAnalyzer.java
@@ -59,6 +59,223 @@ public class SemanticAnalyzer {
 
     //parse block -- semantic
     private void semanticBlock(){
+        ast.addNodeAST("root", "Block");    //add block as root node
+        incrementToken();   //match
+        semanticStatementList();    //statementlist
+        incrementToken();   //match
+    }
+
+    //parse statement list -- semantic
+    private void semanticStatementList(){
+        switch (currentToken.tokenType) {
+            case "PRINT":
+            case "ID":
+            case "TYPE_INT":
+            case "TYPE_STRING":
+            case "TYPE_BOOLEAN":
+            case "WHILE":
+            case "IF":
+            case "OPENING_BRACE":
+                semanticStatement();
+                semanticStatementList();
+                break;
+            default:
+                //do nothing
+                //epsilon case
+                break;
+        }
+    }
+
+    //parse statement -- semantic
+    private void semanticStatement(){
+        //check token type to determine what function to call
+        switch (currentToken.tokenType) {
+            case "PRINT":
+                semanticPrintStatement();
+                break;
+            case "ID":
+                semanticAssignmentStatement();
+                break;
+            case "TYPE_INT":
+            case "TYPE_STRING":
+            case "TYPE_BOOLEAN":
+                semanticVarDecl();
+                break;
+            case "WHILE":
+                semanticWhileStatement();
+                break;
+            case "IF":
+                semanticIfStatement();
+                break;
+            case "OPENING_BRACE":
+                semanticBlock();
+                break;
+            default:
+                //throw error?
+                semanticErrors++;
+                break;
+        }
+    }
+
+    //parse print statement -- semantic
+    private void semanticPrintStatement(){
+        ast.addNodeAST("branch", "Print statement");
+        incrementToken();
+        incrementToken();
+        semanticExpr();
+        incrementToken();
+        ast.moveUpAST();
+    }
+
+    //parse assignment statement -- semantic
+    private void semanticAssignmentStatement(){
+        ast.addNodeAST("branch", "Assignment statement");
+        semanticID();
+        incrementToken();
+        semanticExpr();
+        ast.moveUpAST();
+    }
+
+    //parse variable declaration -- semantic
+    private void semanticVarDecl(){
+        ast.addNodeAST("branch", "Variable declaration");
+        semanticType();
+        semanticID();
+        ast.moveUpAST();
+    }
+
+    //parse while statement -- semantic
+    private void semanticWhileStatement(){
+        ast.addNodeAST("branch", "While statement");
+        incrementToken();
+        semanticBooleanExpr();
+        semanticBlock();
+        ast.moveUpAST();
+    }
+
+    //parse if statement -- semantic
+    private void semanticIfStatement(){
+        ast.addNodeAST("branch", "If statement");
+        incrementToken();
+        semanticBooleanExpr();
+        semanticBlock();
+        ast.moveUpAST();
+    }
+
+    //parse expression -- semantic
+    private void semanticExpr(){
+        //check token type to determine what function to call
+        switch (currentToken.tokenType) {
+            case "DIGIT":
+                semanticIntExpr();
+                break;
+            case "QUOTE":
+                semanticStringExpr();
+                break;
+            case "OPENING_PARENTHESIS":
+            case "BOOL_TRUE":
+            case "BOOL_FALSE":
+                semanticBooleanExpr();
+                break;
+            case "ID":
+                semanticID();
+                break;
+            default:
+                //throw error??
+                semanticErrors++;
+                break;
+        }
+    }
+
+    //parse int expression -- semantic
+    private void semanticIntExpr(){
+        //check if next token is '+'
+        if(tokenStream.get(tokenStreamIndex+1).tokenType.equals("ADD")){
+            semanticDigit();
+            semanticIntOp();
+            semanticExpr();
+        }
+        else{
+            semanticDigit();
+        }
+    }
+
+    //parse string expression -- semantic
+    private void semanticStringExpr(){
+        incrementToken();
+        semanticCharList();
+        incrementToken();
+    }
+
+    //parse boolean expression -- semantic
+    private void semanticBooleanExpr(){
+        //check token type to determine what function to call
+        if(currentToken.tokenType.equals("OPENING_PARENTHESIS")){
+            incrementToken();
+            semanticExpr();
+            semanticBoolOp();
+            semanticExpr();
+            incrementToken();
+        }
+        else{
+            semanticBoolVal();
+        }
+    }
+
+    //parse char list -- semantic
+    private void semanticCharList(){
+        //use a stringbuilder to iterate through a charlist and create 1 string variable
+        StringBuilder charlist = new StringBuilder();
+        while(!currentToken.lexeme.equals("\"")){
+            charlist.append(currentToken.lexeme);
+            incrementToken();
+        }
+        ast.addNodeAST("leaf", charlist.toString());
+    }
+
+    //parse ID -- semantic
+    private void semanticID(){
+        ast.addNodeAST("leaf", currentToken.lexeme);
+        incrementToken();
+    }
+
+    //parse types: int, string, bool -- semantic
+    private void semanticType(){
+        ast.addNodeAST("leaf", currentToken.lexeme);
+        incrementToken();
+    }
+
+    //parse digits 0-9 -- semantic
+    private void semanticDigit(){
+        ast.addNodeAST("leaf", currentToken.lexeme);
+        incrementToken();
+    }
+
+    //parse bool operators -- semantic
+    private void semanticBoolOp(){
+        ast.addNodeAST("branch", currentToken.lexeme);
+        incrementToken();
+        ast.moveUpAST();
+    }
+
+    //parse bool values -- semantic
+    private void semanticBoolVal(){
+        ast.addNodeAST("leaf", currentToken.lexeme);
+        incrementToken();
+    }
+
+    //parse add op (+) -- semantic
+    private void semanticIntOp(){
+        ast.addNodeAST("branch", currentToken.lexeme);
+        incrementToken();
+        ast.moveUpAST();
+    }
+}
+
+
+/*
+    //parse block -- semantic
+    private void semanticBlock(){
         ast.addNodeAST("root", "Block"); //add block node as root
         incrementToken();   //match
         semanticStatementList();    //call statementlist
@@ -243,4 +460,5 @@ public class SemanticAnalyzer {
         }
         ast.addNodeAST("leaf", charlist.toString());
     }
-}
+
+*/
