f5b62d245148e110d4fb787c8964c7bd8e90010e
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-03-30 09:52:33 -0400

    made some progress, but still trying to figure everything out

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -12,12 +12,14 @@ public class SymbolTable {
     SymbolTableNode current;   //pointer to the current node
     int currentScope;   //scope pointer
     String traversal;   //string to hold symbol table traversal
+    int STErrors;
 
     //Symbol Table constructor -- initializes variables
     public SymbolTable(){
         this.root = null;
         this.current = null;
         this.currentScope = 0;
+        this.STErrors = 0;
         this.traversal = "";
     }   
 
@@ -57,6 +59,7 @@ public class SymbolTable {
                 break;
             default:
                 //throw error?
+                STErrors++;
                 break;
         }
 
@@ -64,6 +67,111 @@ public class SymbolTable {
         for(Node child : node.children){
             inOrder(child);
         }
+    }
+
+    //opens a new scope within the symbol table
+    private void openScope(){
+       if(root == null){
+            addNodeSymbolTable(currentScope);
+       }
+       else{
+            currentScope++;
+            addNodeSymbolTable(currentScope);
+       }
+    }
+
+    //closes the most recently opened scope
+    private void closeScope(){
+        //if the current scope is not the root node then move the pointer to its parent
+        if(current.scope != 0 && current.parent != null){
+            current = current.parent;
+            currentScope--;
+        }
+    }
+
+    //add symbol to the current nodes hash table
+    private void STVarDecl(Node currentNode){
+        String varID = currentNode.children.get(0).name;
+        String varType = currentNode.children.get(1).name;
+        addSymbol(varID, varType);
+    }
+
+    //lookup the symbol and check types
+    private void STAssignmentStatement(Node currentNode){
+        Symbol temp = lookupSymbol(currentNode.children.get(0).name);
+        if(temp == null){
+            //throw error
+            STErrors++;
+            return;
+        }
+        else{
+            switch (temp.type) {
+                case "int":
+                    //if to check what 2nd child node is ( digit, + or id)
+                    break;
+                case "string":
+                    // just check 2nd child node for " or chars "
+                    break;
+                case "boolean":
+                    //if to check what 2nd child node is (boolval and expr)
+                    break;
+                default:
+                    //throw error
+                    break;
+            }
+            //switch case for symbol type?
+            //eval expression
+        }
+        temp.isINIT = true;
+    }
+
+    //lookup symbol to check scope
+    private void STPrintStatement(Node currentNode){
+        Symbol temp = lookupSymbol(currentNode.children.get(0).name);
+        if(temp == null){
+            //throw error
+            STErrors++;
+            return;
+        }
+    }
+
+
+    private void STWhileStatement(){}
+    private void STIfStatement(){}
+    private void STBoolOP(){}
+    private void STIntOP(Node nextNode){
+        //recursive to?? -- a = 1+1+1+1 etc
+    }
+
+    //looks up a symbol to check if it was ever used
+    private boolean checkUsed(){
+        //lookup symbol and check if used
+        //return true or false
+    }
+
+    //looks up a symbol to check if it was ever initialized
+    private boolean checkINIT(){
+        //lookup symbol and check if init
+        //return true or false
+    }
+
+    //adds a symbol to the symbol table
+    private void addSymbol(String id, String type){
+        Symbol newSymbol = new Symbol(id, type, currentScope);
+        current.symbols.put(newSymbol.name, newSymbol);
+    }
+
+    //looks up a symbol within each ST/scope and returns it
+    private Symbol lookupSymbol(String id){
+        Symbol temp = null;
+        SymbolTableNode tempNode = current;
+        int tempPointer = currentScope;
+        while(tempPointer != -1 && temp == null){
+            temp = tempNode.symbols.get(id);
+            tempPointer--;
+            tempNode = tempNode.parent;
+        }
+        return temp;
     }
 
     //adds a node to the symbol table
@@ -84,66 +192,6 @@ public class SymbolTable {
         }
         //move current to new node
         current = newNode;
-    }
-
-    //opens a new scope within the symbol table
-    private void openScope(){
-       if(root == null){
-            addNodeSymbolTable(currentScope);
-       }
-       else{
-            currentScope++;
-            addNodeSymbolTable(currentScope);
-       }
-    }
-
-    //closes the most recently opened scope
-    private void closeScope(){
-        //if the current scope is not the root node then move the pointer to its parent
-        if(current.scope != 0){
-            current = current.parent;
-            currentScope--;
-        }
-    }
-
-    //add symbol to the current nodes hash table
-    private void STVarDecl(Node currentNode){
-        String varID = currentNode.children.get(0).name;
-        String varType = currentNode.children.get(1).name;
-        addSymbol(varID, varType);
-    }
-
-    //lookup the symbol and check types
-    private void STAssignmentStatement(Node currentNode){
-        Symbol temp = lookupSymbol(currentNode.children.get(0).name);
-        //eval expression
-        temp.isINIT = true;
-    }
-
-    //lookup symbol to check scope
-    private void STPrintStatement(Node currentNode){
-        Symbol temp = lookupSymbol(currentNode.children.get(0).name);
-        //check scope??
-    }
-
-
-    private void STWhileStatement(){}
-    private void STIfStatement(){}
-    private void STBoolOP(){}
-    private void STIntOP(){}
-    private void checkUsed(){}
-    private void checkINIT(){}
-
-    //adds a symbol to the symbol table
-    private void addSymbol(String id, String type){
-        Symbol newSymbol = new Symbol(id, type, currentScope);
-        current.symbols.put(newSymbol.name, newSymbol);
-    }
-
-    //looks up a symbol within the symbol table and returns it
-    private Symbol lookupSymbol(String id){
-        return current.symbols.get(id);
-        //have to put code to check all scopes
     }
 
 
