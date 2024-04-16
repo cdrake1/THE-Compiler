e34a1f53b1b381781374985d8d10c1aef50ebd5c
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2024-04-15 22:40:23 -0400

    running into an issue when trying to get a value from the hash table. Is it because the objects are different even though the content is the same?

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -7,13 +7,13 @@ import java.util.Hashtable;
 
 //Collin Drakes Code Generator
 public class CodeGenerator {
-    String[] opCodes;    //stores the code, stack, heap
-    Hashtable<String, staticTableVariable> varTable;    //the variable table
-    Hashtable<String, String> stringTable; //the string table -- keeps track of strings in the heap
+    String[] memory;    //stores the code, stack, heap
+    Hashtable<staticTableKey, staticTableVariable> staticTable;    //the variable table
     Hashtable<String, branchTableVariable> branchTable; //the branch table
     int currentIndex;   //keeps track of the current index of the opCodes array
     int tempCounter;    //keeps track of temp number for variable table "T0XX"
     int currentScope;   //keeps track of what scope we are in
+    boolean rootCreated;
 
     int codePointer;    //pointer to where the code starts
     int stackPointer;   //pointer to where the stack starts -- directly after code
@@ -27,12 +27,13 @@ public class CodeGenerator {
 
     //Code Generator constructor -- initializes variables
     public CodeGenerator(AST ast, SymbolTable ST, int programCounter){
-        this.opCodes = new String[256];
-        this.varTable = new Hashtable<>();
+        this.memory = new String[256];
+        this.staticTable = new Hashtable<>();
         this.branchTable = new Hashtable<>();
         this.currentIndex = 0;
         this.tempCounter = 0;
         this.currentScope = 0;
+        this.rootCreated = false;
 
 
         this.codePointer = 0;
@@ -53,13 +54,13 @@ public class CodeGenerator {
 
     //starts the code generation process
     public void startCodeGen(){
-        initArray();   //init all index of the opCodes array to 00
+        initMemory();   //init all index of the opCodes array to 00
         inOrder(ast.root);
 
         if(codeGenErrors == 0){
             codeGeneratorLog("Code Generation Complete... Errors: " + codeGenErrors + "\n");
-            for(int i = 0; i < opCodes.length; i++){
-                System.out.print(" " + opCodes[i]);
+            for(int i = 0; i < memory.length; i++){
+                System.out.print(" " + memory[i]);
             }
         }
         else{
@@ -68,10 +69,26 @@ public class CodeGenerator {
     }
 
     //initializes all of memory (code, stack, and heap) to 00
-    private void initArray(){
-        for(int i = 0; i < opCodes.length; i++){
-            opCodes[i] = "00";
+    private void initMemory(){
+        for(int i = 0; i < memory.length; i++){
+            memory[i] = "00";
         }
+
+        //---add true false to end of heap---
+
+        //--false--
+        memory[244] = "66"; //f
+        memory[245] = "61"; //a
+        memory[246] = "6C"; //l
+        memory[247] = "73"; //s
+        memory[248] = "65"; //e
+        memory[249] = "00"; //0
+        //--true--
+        memory[250] = "74"; //t
+        memory[251] = "72"; //r
+        memory[252] = "75"; //u
+        memory[253] = "65"; //e
+        memory[254] = "00"; //0
     }
 
     //function for the depth first in order traversal of the AST
@@ -84,7 +101,7 @@ public class CodeGenerator {
         //check the name of the tokens to determine which function to call
         switch (node.name) {
             case "Block":
-                currentScope++; //increment scope
+                codeGenOpenScope();
                 break;
             case "Variable declaration":
                 codeGenVarDecl(node);
@@ -118,6 +135,16 @@ public class CodeGenerator {
         System.out.println(node.name);  //test to output nodes
     }
 
+    //increment the scope pointer
+    private void codeGenOpenScope(){
+        if(rootCreated == false){
+            rootCreated = true;
+        }
+        else{
+            currentScope++; //increment scope
+        }
+    }
+
     //produces the op codes for variable declarations
     private void codeGenVarDecl(ASTNode currentNode){
         ASTNode typeNode = currentNode.children.get(0);
@@ -131,10 +158,17 @@ public class CodeGenerator {
             //unsure how to do this
         }
         else{
-            staticTableVariable temp = new staticTableVariable("T" + Integer.toString(tempCounter), idNode.name, currentScope, staticTableOffset);
+            //add the temp address to the memory
+            String tempAddress = "T" + Integer.toString(tempCounter);
+            addOpCode(tempAddress);
+
+            //add the variable to the static variable table
+            System.out.println(idNode.name +" " + currentScope);
+            staticTableKey tempKey = new staticTableKey(idNode.name, currentScope);
+            staticTableVariable tempVar = new staticTableVariable(tempAddress, idNode.name, currentScope, staticTableOffset);
             tempCounter++;
             staticTableOffset++;
-            varTable.put("T" + Integer.toString(tempCounter), temp);
+            staticTable.put(tempKey, tempVar);
         }
         addOpCode("00");
     }
@@ -143,15 +177,24 @@ public class CodeGenerator {
     private void codeGenAssignmentStatement(ASTNode currentNode){
         ASTNode idNode = currentNode.children.get(0);
         ASTNode exprNode = currentNode.children.get(1);
+
+        System.out.println(idNode.name + " name scope " + currentScope);
+
+        //get the variables temp address
+        System.out.println(idNode.name +" " + currentScope);
+        staticTableKey tempKey = new staticTableKey(idNode.name, currentScope);
+        staticTableVariable tempVar = staticTable.get(tempKey);
+        if(tempVar == null){
+            System.out.println("Its null why?");
+        }
         
         switch (exprNode.token.tokenType) {
             case "ID":
+                addOpCode("A9");
                 break;
             case "DIGIT":
                 addOpCode("A9");
-                //something
-                addOpCode("8D");
-                //temp address
+                addOpCode("0" + exprNode.name);
                 break;
             case "ADD":
                 break;
@@ -168,6 +211,9 @@ public class CodeGenerator {
             default:
                 break;
         }
+        addOpCode("8D");
+        //addOpCode(leftNodetempAddress);    //temp
+        addOpCode("00");
     }
 
     private void codeGenPrintStatement(ASTNode currentNode){}
@@ -176,12 +222,12 @@ public class CodeGenerator {
 
     //adds opcodes to the array and increments the index
     private void addOpCode(String opCode){
-        if(currentIndex > opCodes.length){
-            codeGeneratorLog("ERROR! INVALID MEMORY LOCATION... INDEX EVALUATES AS GREATER THAN THE MEMORY LENGHT");
+        if(currentIndex > memory.length){
+            codeGeneratorLog("ERROR! INVALID MEMORY LOCATION... INDEX EVALUATES AS GREATER THAN THE MEMORY LENGTH");
             codeGenErrors++;
         }
         else{
-            opCodes[currentIndex] = opCode;
+            memory[currentIndex] = opCode;
             currentIndex++;
         }
     }
--- src/staticTableVariable.java
+++ src/staticTableVariable.java
@@ -3,18 +3,30 @@
     Creates objects for the code generation variable table
 */
 
-//The static table variable class!
+//The static table variable object class!
 public class staticTableVariable {
-    String temp;    //store the accumulator in a temp location/address TXXX
+    String tempAddress;    //store the accumulator in a temp location/address TX
     String var; //what variable is this?
     int scope;   //keeps track of the scope the variable is located in
     int offset; //number of offset following code section of the array (location of variable stored)
 
     //Static table variable constructor -- initializes all variables
     public staticTableVariable(String temp, String var, int scope, int offset){
-        this.temp = temp;
+        this.tempAddress = temp;
         this.var = var;
         this.scope = scope;
         this.offset = offset;
+    }
+}
+
+//The static table key object class!
+class staticTableKey{
+    String ID;  //the variable name
+    int scope;  //the scope its in
+
+    //static table key constructor
+    public staticTableKey(String id, int scope){
+        this.ID = id;
+        this.scope = scope;
     }
 }
